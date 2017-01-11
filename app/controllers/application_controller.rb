fbd828b237a76c9aeae5cac446afdb92a36b142d
bittu-choudhary <bittu@moldedbits.com>
2017-01-11 23:39:50 +0530

    adding check response for emailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,18 +20,18 @@ class ApplicationController < ActionController::Base
     content = data.text.split("/text") if data.text
     get_message = Slack::RealTime::Client.new
     if data.command == "/email" && (content.length == 2) && !content.first.blank? && !content.last.blank?
-      get_message.message user: data.user_id, text: "Do you want to send this email? subject -> #{content.first.strip}, body -> #{content.last.strip}. Type y/n")
+      get_message.message user: data.user_id, text: "Do you want to send this email? subject -> #{content.first.strip}, body -> #{content.last.strip}. Type y/n"
       get_message.on :message do |data|
         if (data.text == 'y' || data.text == 'Y')
           CalvinMailer.inform_channel(from_name, from_email, members_emails, content.first.strip, content.last.strip).deliver
         elsif (data.text == 'n' || data.text == 'N')
-          get_message.message user: data.user_id, text: "Email not sent.")
+          get_message.message user: data.user_id, text: "Email not sent."
         else
-          get_message.message user: data.user_id, text: "Enter correct value.")
+          get_message.message user: data.user_id, text: "Enter correct value."
         end
       end
     else
-      get_message.message user: data.user_id, text: "Wrong email format.(Follow /email <subject> /text <body>)")
+      get_message.message user: data.user_id, text: "Wrong email format.(Follow /email <subject> /text <body>)"
     end
   end
 end
