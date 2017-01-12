9acde3dd3527bad9423b4788360cfa2cc314ba49
bittu-choudhary <bittu@moldedbits.com>
2017-01-12 06:01:43 +0530

    adding check response for emailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,40 +22,40 @@ class ApplicationController < ActionController::Base
   # end
 
   def create
-    data  =OpenStruct.new params
-    client = Slack::Web::Client.new
-    from_name = client.users_info(user: data.user_id).user.profile.real_name if data.user_id
-    from_email = client.users_info(user: data.user_id).user.profile.email if data.user_id
-    members = client.channels_info(channel: data.channel_id).channel.members.to_a - [data.user_id] if data.channel_id
-    members_emails = []
-    if !members.blank?
-      members.each do |member|
-        members_emails << client.users_info(user: member).user.profile.email if client.users_info(user: member).user.profile.email
-      end
-    end
-    content = data.text.split("/text") if data.text
-    get_message = Slack::RealTime::Client.new
-    if data.command == "/email" && (content.length == 2) && !content.first.blank? && !content.last.blank?
-      client.chat_postMessage(channel: data.user_id, text: "Do you want to send this email? subject -> #{content.first.strip}, body -> #{content.last.strip}. Type y/n", as_user: true)
-      state = 1
-      get_message.on :message do |data|
-        # get_message.message user: data.user_id, text: "Do you want to send this email? subject -> #{content.first.strip}, body -> #{content.last.strip}. Type y/n" if state == 1
-        # state = 2
-        if (data.text == 'y' || data.text == 'Y')
-          CalvinMailer.inform_channel(from_name, from_email, members_emails, content.first.strip, content.last.strip).deliver
-        elsif (data.text == 'n' || data.text == 'N')
-          get_message.message channel: data.channel, text: "Email not sent."
-        else
-          get_message.message channel: data.channel, text: "Enter correct value."
+    data  = OpenStruct.new params
+    if data.token != ENV['SLACK_EMAIL_BOT']
+      client = Slack::Web::Client.new
+      from_name = client.users_info(user: data.user_id).user.profile.real_name if data.user_id
+      from_email = client.users_info(user: data.user_id).user.profile.email if data.user_id
+      members = client.channels_info(channel: data.channel_id).channel.members.to_a - [data.user_id] if data.channel_id
+      members_emails = []
+      if !members.blank?
+        members.each do |member|
+          members_emails << client.users_info(user: member).user.profile.email if client.users_info(user: member).user.profile.email
         end
       end
+      content = data.text.split("/text") if data.text
+      get_message = Slack::RealTime::Client.new
+      if data.command == "/email" && (content.length == 2) && !content.first.blank? && !content.last.blank?
+        client.chat_postMessage(channel: data.user_id, text: "Do you want to send this email? subject -> #{content.first.strip}, body -> #{content.last.strip}. Type shoot/nope", as_user: true)
+        state = 1
+        get_message.on :message do |data|
+          if (data.text.downcase == 'shoot')
+            CalvinMailer.inform_channel(from_name, from_email, members_emails, content.first.strip, content.last.strip).deliver
+          elsif (data.text.downcase == 'nope')
+            get_message.message channel: data.channel, text: "Email not sent."
+          else
+            get_message.message channel: data.channel, text: "Enter correct value."
+          end
+        end
 
-      Thread.new do
-        get_message.start!
+        Thread.new do
+          get_message.start!
+        end
+
+      else
+        client.chat_postMessage(channel: data.user_id, text: "Wrong email format.(Follow /email <subject> /text <body>)", as_user: true)
       end
-
-    else
-      client.chat_postMessage(channel: data.user_id, text: "Wrong email format.(Follow /email <subject> /text <body>)", as_user: true)
     end
   end
 end
