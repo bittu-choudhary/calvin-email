ae1bfd1ea8a7be78d4845bf3dd95b08120e6ff3b
bittu-choudhary <bittu@moldedbits.com>
2017-01-12 07:09:29 +0530

    adding check response for emailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,15 +44,15 @@ class ApplicationController < ActionController::Base
           chat_channel = nil
           client.im_list.ims.each{|direct_channels| chat_channel = direct_channels.id if direct_channels.user == data.user}
           if data.channel == chat_channel && (data.user != "U3FCSJZ6D") #calvin user id
-            if (data.text.downcase == 'shoot') && (state == "email_prepared")
-              CalvinMailer.inform_channel(from_name, from_email, members_emails, content.first.strip, content.last.strip).deliver
-              get_message.message channel: data.channel, text: "Done. Have a nice day."
-              state = nil
+            if (data.text.downcase == 'shoot') && state.nil?
+              get_message.message channel: data.channel, text: "Nothing to shoot. Queue a email first."
             elsif (data.text.downcase == 'nope') && (state == "email_prepared")
               get_message.message channel: data.channel, text: "Email not sent."
               state = nil
-            elsif (data.text.downcase == 'shoot') && state.nil?
-              get_message.message channel: data.channel, text: "Nothing to shoot. Queue a email first."
+            elsif (data.text.downcase == 'shoot') && (state == "email_prepared")
+              CalvinMailer.inform_channel(from_name, from_email, members_emails, content.first.strip, content.last.strip).deliver
+              get_message.message channel: data.channel, text: "Done. Have a nice day."
+              state = nil
             else
               get_message.message channel: data.channel, text: "Sorry, i didn't understand that"
             end
